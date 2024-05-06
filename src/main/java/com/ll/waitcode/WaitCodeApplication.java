307babab8b35f946e2a39f849419ffc5f9d3ae44
전화철 <lunaris0206@naver.com>
2024-05-06 23:55:22 +0900

    Initial commit

---
+++ src/main/java/com/ll/waitcode/WaitCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.waitcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/waitcode/WaitCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.waitcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
